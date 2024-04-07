8da3cf90025f2187770fc887dae7a440ed2ffdac
Lasse Rolstad <lasse.rolstad@glasspaper.no>
2024-04-07 19:48:00 +0200

    Added extension to register with DI and a simple unit test to make sure it will resolve without error.

---
+++ src/HierarchyBuilder/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+﻿using HierarchyBuilder.Abstractions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HierarchyBuilder.Extensions
+{
+    /// <summary>
+    /// Service collection extensions
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Add the hierarchy builder to the service collection
+        /// </summary>
+        /// <param name="services"></param>
+        /// <param name="serviceLifetime"></param>
+        /// <returns></returns>
+        public static IServiceCollection AddHierarchyBuilder(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Transient)
+        {
+            services.Add(new ServiceDescriptor(typeof(IHierarchyTransformer), typeof(Transformer), serviceLifetime));
+            return services;
+        }
+    }
+}
---
+++ test/HierarchyBuilder.UnitTest/Extensions/ServiceCollectionExtensionTests.cs
@@ -0,0 +1,30 @@
+﻿using FluentAssertions;
+using HierarchyBuilder.Abstractions;
+using HierarchyBuilder.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HierarchyBuilder.UnitTest.Extensions;
+
+public class ServiceCollectionExtensionTests
+{
+    private readonly IServiceProvider _provider;
+
+    public ServiceCollectionExtensionTests()
+    {
+        var collection = new ServiceCollection();
+        collection.AddHierarchyBuilder();
+        _provider = collection.BuildServiceProvider();
+    }
+    
+    [Fact]
+    public void AddHierarchyBuilder_Should_Resolve_IHierarchyTransformer()
+    {
+        // Arrange
+        // Act
+        var transformer = _provider.GetService<IHierarchyTransformer>();
+        
+        // Assert
+        transformer.Should().NotBeNull();
+        transformer.Should().BeAssignableTo<Transformer>();
+    }
+}
